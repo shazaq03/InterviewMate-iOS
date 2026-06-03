2e20210df44c92174b291877a8aa8ca8da407ca1
siirus <sisrius@gmail.com>
2026-06-03 17:46:12 +0800

    Initial Commit

---
+++ InterviewMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InterviewMate
+//
+//  Created by user298368 on 6/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InterviewMate/InterviewMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewMateApp.swift
+//  InterviewMate
+//
+//  Created by user298368 on 6/3/26.
+//
+
+import SwiftUI
+
+@main
+struct InterviewMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
